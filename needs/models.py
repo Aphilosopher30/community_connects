83416d4eaba1905ab9cc1b9ffdeb3b598fbdcd57
Andrew Shafer <73140608+Aphilosopher30@users.noreply.github.com>
2022-01-25 21:20:30 -0700

    set up db needs

--- needs/models.py
+++ needs/models.py
@@ -3,17 +3,15 @@ from django.db import models
 # Create your models here.
 
 class Need(models.Model):
-    title = models.CharField()
-    description = models.CharField()
-    start_time = models.CharField()
-    end_time = models.CharField()
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=500)
+    start_time = models.CharField(max_length=200)
+    end_time = models.CharField(max_length=200)
 
-    city = models.CharField()
-    state = models.CharField()
-    zip_code = models.CharField()
-    street_address = models.CharField()
-    street_address = models.CharField()
-    street_address = models.CharField()
+    city = models.CharField(max_length=200)
+    state = models.CharField(max_length=200)
+    zip_code = models.CharField(max_length=200)
+
     # t.integer "supporters_needed"
     # t.integer "status"
     # t.datetime "created_at", null: false
