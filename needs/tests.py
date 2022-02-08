92915f9909f96731c3cd2c02d72e447191e071da
Andrew Shafer <73140608+Aphilosopher30@users.noreply.github.com>
2022-02-08 14:52:47 -0700

    add test for needs moddle

--- needs/tests.py
+++ needs/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from needs.models import Need
+
 
 # Create your tests here.
+
+class TestNeedModel(TestCase):
+    def test_model_str(self):
+        title = Need.objects.create(title="Django Testing")
+        self.assertEqual(str(title), "Django Testing")
