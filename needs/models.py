ac70d60b4d0151cf1985c093ec2189457935f831
Andrew Shafer <73140608+Aphilosopher30@users.noreply.github.com>
2022-03-17 22:24:54 -0600

    update database

--- needs/models.py
+++ needs/models.py
@@ -4,16 +4,17 @@ from django.db import models
 
 class Need(models.Model):
     title = models.CharField(max_length=200)
-    description = models.CharField(max_length=500)
-    start_time = models.CharField(max_length=200)
+    description = models.CharField(max_length=1000)
 
     supporters_needed = models.IntegerField(null=True)
+    time_frame = models.CharField(max_length=200)
 
-    # end_time = models.CharField(max_length=200)
-    # city = models.CharField(max_length=200)
-    # state = models.CharField(max_length=200)
-    # zip_code = models.CharField(max_length=200)
-    # supporters_needed = models.IntegerField(null=True)
+    notes = models.CharField(max_length=1000, null=True)
+    adress = models.CharField(max_length=200, null=True)
+    contact_info = models.CharField(max_length=200, null=True)
+
+    # status:  (enum. [active, inactive])
+
 
     def __str__(self):
         return self.title
@@ -37,5 +38,13 @@ class NeedCategories(models.Model):
 # class Supporters(models.Model):
 #     name = models.CharField(max_length=200)
 #     email = models.CharField(max_length=200)
+
+# class what they signed up for supporters_need
+
 # info on makeing a
 # https://hackernoon.com/using-enum-as-model-field-choice-in-django-92d8b97aaa63
+
+
+
+
+#vonuntere for now add paied connection later.
