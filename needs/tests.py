22cb57a4a5f7da7343a9441117d94e7b06784d14
Andrew Shafer <73140608+Aphilosopher30@users.noreply.github.com>
2022-03-17 23:03:48 -0600

    add tests for database

--- needs/tests.py
+++ needs/tests.py
@@ -7,20 +7,27 @@ from needs.models import Categories
 
 class TestNeedModel(TestCase):
     def test_model_str(self):
-        test = Need.objects.create(title="Django Testing",
-                                    description="this is a test",
-                                    start_time="noon",
-                                    supporters_needed = 5
+        test = Need.objects.create(title = "Django Testing",
+                                    description = "this is a test",
+                                    time_frame = "noon",
+                                    supporters_needed = 5,
+                                    notes = "notes notes notes",
+                                    adress = "here",
+                                    contact_info = "phone number or email"
                                     )
 
         self.assertEqual(str(test), "Django Testing")
         self.assertEqual(test.title, "Django Testing")
         self.assertEqual(test.description, "this is a test")
-        self.assertEqual(test.start_time, "noon")
+        self.assertEqual(test.time_frame, "noon")
         self.assertEqual(test.supporters_needed, 5)
+        self.assertEqual(test.notes, "notes notes notes")
+        self.assertEqual(test.adress, "here")
+        self.assertEqual(test.contact_info, "phone number or email")
 
 
 class TestCategoriesModel(TestCase):
     def test_model_str(self):
         tag = Categories.objects.create(tag="tag testing")
+
         self.assertEqual(str(tag), "tag testing")
