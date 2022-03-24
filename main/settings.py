5ad41686e8648d7dc20627f2936bf201a2e63ca3
Andrew Shafer <73140608+Aphilosopher30@users.noreply.github.com>
2022-03-23 19:52:56 -0600

    install graph ql

--- main/settings.py
+++ main/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'needs.apps.NeedsConfig', 
+    'needs.apps.NeedsConfig',
+    'graphene_django',
 ]
 
 MIDDLEWARE = [
