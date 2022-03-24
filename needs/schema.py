4d18e9a43f7909c058900e8b5d9e2b02f6078754
Andrew Shafer <73140608+Aphilosopher30@users.noreply.github.com>
2022-03-23 21:02:50 -0600

    set up needs graphql

---
+++ needs/schema.py
@@ -0,0 +1,8 @@
+import graphene
+from graphene_django import DjangoObjectType
+from .models import Books
+
+class BooksTypes(DjangoObjectType):
+    class Meta:
+        model = Need
+        fields = ("id", "title", "description", "supporters_needed", "time_frame", "notes", "location", "contact_info")
